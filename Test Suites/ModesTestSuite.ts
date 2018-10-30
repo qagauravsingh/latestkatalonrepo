a3203aba1daf6791ddf514310710edb08c56528c
qagau <qagau@DESKTOP-7B2OP92>
2018-10-31 00:15:54 +0530

    latest changes

---
+++ Test Suites/ModesTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ModesTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-30T01:24:17</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f14612ff-786d-47ff-a205-4f2af31de3cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>4a7eb45e-d93c-4382-8d28-7f09b7df4482</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RecordMode</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
